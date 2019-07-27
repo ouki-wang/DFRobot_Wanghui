8d7900cede7ce36c55ad36df8193bc92c2df2c12
ouki-wang <ouki-wang@dfrobot.com>
2019-07-27 11:45:59 +0800

    add licence keywords

--- examples/setNameAge/setNameAge.ino
+++ examples/setNameAge/setNameAge.ino
@@ -1,12 +1,17 @@
 #include <DFRobot_Wanghui.h>
+DFRobot_Wanghui wh;
 
 void setup(void)
 {
   Serial.begin(115200);
   delay(100);
   Serial.println("hello DFRobot");
-  DFRobot_Wanghui wh;
+  wh.setAge(18);
+  wh.setName("Alexander");
+  Serial.print("name=");Serial.println(wh.getName());
+  Serial.print("age=");Serial.println(wh.getAge());
 }
+
 void loop(void)
 {
   
